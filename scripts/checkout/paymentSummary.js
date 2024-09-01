b56669b5d7edc4b3d04a70a3f2d8c1168c34f1a7
priyanshuraj <rajp58425@gmail.com>
2024-09-02 01:02:08 +0530

    add jamsmine testing framework

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -6,6 +6,7 @@ import formatCurrency from '../utility/money.js';
 export function renderPaymentSummary(){
   let productPriceCents = 0;
   let shippingPriceCents = 0;
+  let cartItemQuantity = 0;
 
   cart.forEach((cartItem) => {
     const product = getProduct(cartItem.productId);
@@ -13,6 +14,7 @@ export function renderPaymentSummary(){
 
     const deliveryOption =  getDeliveryOption(cartItem.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
+    cartItemQuantity ++;
   });
 
   const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
@@ -25,7 +27,7 @@ export function renderPaymentSummary(){
       </div>
 
       <div class="payment-summary-row">
-        <div>Items (3):</div>
+        <div>Items (${cartItemQuantity}):</div>
         <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
       </div>
 
